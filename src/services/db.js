b2ae72e8e051be3c749a7f1d344894c9330c5e60
TheHackBoxCampus <millercampusdeveloper@gmail.com>
2023-07-29 21:17:52 -0500

    feat: 🗄️ Conexion ``mongoDB``
    Se creo la conexion a la coleccion de datos ``taskUser``, en este caso como existia una coleccion con ese nombre ``mongo`` la creo
    
    footer: mongoDB, mongoose

--- src/services/db.js
+++ src/services/db.js
@@ -1,5 +1,15 @@
 import mongoose from "mongoose";
+import enviroments from "../config/enviroments.js";
 
-const conx = async () => await mongoose.connect('mongodb://localhost/taskuser')
+const conx = async () => {
+   try {
+       const host = enviroments.HOST;
+       await mongoose.connect(`mongodb://${host}/taskUser`);
+       console.log(">> DB connect!")
+   }catch (err) {
+      console.log(err)
+   }
+
+}
 
 export default conx
