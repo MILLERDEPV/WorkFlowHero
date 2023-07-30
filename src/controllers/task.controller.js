1990fa66a3c9e8f4406e850ffd3623afe49f7df4
TheHackBoxCampus <millercampusdeveloper@gmail.com>
2023-07-29 23:31:36 -0500

    feat: 🕹️ Modificacion Controlador ``task``
    Se agregaron las funciones de forma inicial para el CRUD de las tareas
    
    footer: CRUD

---
+++ src/controllers/task.controller.js
@@ -0,0 +1,16 @@
+import Task from "../models/task.model.js";
+
+const getTasks = async (req, res) => {}
+const getTask = async (req, res) => {}
+const createTask = async (req, res) => {}
+const updateTask = async (req, res) => {}
+const deteleTask = async (req, res) => {}
+
+export {
+    getTask,
+    getTasks,
+    createTask,
+    updateTask,
+    deteleTask
+}
+
